2de7427aead4ee3646e05822b950dd70f5a61a07
panosjapan7 <panosjapan@gmail.com>
2022-07-08 09:02:34 +0200

    head(), title, meta

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h2>Welcome to the best corny dad jokes app on the web</h2>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+    head() {
+        return {
+            title: "Welcome to dad jokes",
+            meta: [
+                {
+                    hid: "description",
+                    name: "description",
+                    content: "Best place for corny dad jokes"
+                }
+            ]
+        }
+    }
 }
 </script>
