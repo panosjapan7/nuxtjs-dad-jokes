d9fed18bf7cb8a0805f2491154e3adfa809af489
panosjapan7 <panosjapan@gmail.com>
2022-07-08 09:11:32 +0200

    Joke component

---
+++ components/Joke.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="joke">
+    <p>{{ joke }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Joke",
+    props: ["joken", "id"],
+}
+</script>
+
+<style>
+.joke {
+    padding: 1rem;
+    border: 1px dotted gray;
+    margin: 1rem 0;
+}
+
+</style>
