1315506087aef25bb292e92fb3c44ae5c79f815c
panosjapan7 <panosjapan@gmail.com>
2022-07-08 09:01:59 +0200

    Jokes page

---
+++ pages/jokes/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>Jokes</div>
+</template>
+
+<script>
+export default {
+  head() {
+        return {
+            title: "Dad Jokes",
+            meta: [
+                {
+                    hid: "description",
+                    name: "description",
+                    content: "Best place for corny dad jokes"
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
